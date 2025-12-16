b41241eb52972956507dacb70d3f868a53168eb1
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-12-16 18:55:57 -0300

    feat: adiciona implementação e testes do algoritmo Bubble Sort

---
+++ lib/sorting_algorithms/bubble_sort.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module SortingAlgorithms
+  module BubbleSort
+    def self.call(array)
+      result = array.dup
+      swapped = true
+      while swapped
+        swapped = false
+        (0...result.length - 1).each do |i|
+          if result[i] > result[i + 1]
+            result[i], result[i + 1] = result[i + 1], result[i]
+            swapped = true
+          end
+        end
+      end
+      result
+    end
+  end
+end
