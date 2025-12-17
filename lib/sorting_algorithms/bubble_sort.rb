3403e9014ec3b1e3bc013ff6da2c2e920c3f65fd
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-12-16 23:20:36 -0300

    feat: refatora a implementação do Bubble Sort para utilizar a sintaxe de classe

--- lib/sorting_algorithms/bubble_sort.rb
+++ lib/sorting_algorithms/bubble_sort.rb
@@ -2,19 +2,21 @@
 
 module SortingAlgorithms
   module BubbleSort
-    def self.call(array)
-      result = array.dup
-      swapped = true
-      while swapped
-        swapped = false
-        (0...result.length - 1).each do |i|
-          if result[i] > result[i + 1]
-            result[i], result[i + 1] = result[i + 1], result[i]
-            swapped = true
+    class << self
+      def call(array)
+        result = array.dup
+        swapped = true
+        while swapped
+          swapped = false
+          (0...result.length - 1).each do |i|
+            if result[i] > result[i + 1]
+              result[i], result[i + 1] = result[i + 1], result[i]
+              swapped = true
+            end
           end
         end
+        result
       end
-      result
     end
   end
 end
