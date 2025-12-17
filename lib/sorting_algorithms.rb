d74c863563b21d7501d55eaa546d6e149c95a0e4
Fernando José <fernando.tecnicoinfo7@gmail.com>
2025-12-17 10:17:30 -0300

    feat(main): add Merge Sort algorithm call

--- lib/sorting_algorithms.rb
+++ lib/sorting_algorithms.rb
@@ -3,6 +3,7 @@
 require_relative "sorting_algorithms/bubble_sort"
 require_relative "sorting_algorithms/insertion_sort"
 require_relative "sorting_algorithms/selection_sort"
+require_relative "sorting_algorithms/merge_sort"
 
 module SortingAlgorithms
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 require_relative "lib/sort"
+puts SortingAlgorithms::MergeSort.call([5, 2, 8, 3, 1])
